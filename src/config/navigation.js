4c564607cc6a1a34b8c9f12466dcc2139f561040
Vincent Brocheton <vinc.brocheton@gmail.com>
2026-07-02 12:38:00 +0200

    Ajout de la navigation

---
+++ src/config/navigation.js
@@ -0,0 +1,64 @@
+export const navigation = [
+
+    {
+        label: "Accueil",
+        route: "/",
+        icon: "home",
+    },
+
+    {
+        label: "Créer son personnage",
+        route: "/creation",
+        icon: "user-plus",
+    },
+
+    {
+        separator: true,
+    },
+
+    {
+        label: "Clans",
+        collection: "clans",
+        icon: "shield",
+    },
+
+    {
+        label: "Disciplines",
+        collection: "disciplines",
+        icon: "sparkles",
+    },
+
+    {
+        label: "Techniques",
+        collection: "techniques",
+        icon: "swords",
+    },
+
+    {
+        label: "Rituels",
+        collection: "rituels",
+        icon: "scroll",
+    },
+
+    {
+        label: "Compétences",
+        collection: "competences",
+        icon: "brain",
+    },
+
+    {
+        label: "Atouts & Handicaps",
+        collection: "atouts",
+        icon: "star",
+    },
+
+    {
+        separator: true,
+    },
+
+    {
+        label: "Règles",
+        route: "/regles",
+        icon: "book-open",
+    },
+];
